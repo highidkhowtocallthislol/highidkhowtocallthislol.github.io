6a26c7cc8a4177ff855d9270a2c1c34d529d269e
highskyguy1 <70930181+highidkhowtocallthislol@users.noreply.github.com>
2024-08-24 21:50:12 +0800

    Add files via upload

---
+++ net/highskyguy/highmod/1.2.3b6-1.20.2/index.php
@@ -0,0 +1,20 @@
+
+<?php
+$title = "Index of /1.2.3b6-1.20.2";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo($title); ?></title>
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/maven.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <h1><?php echo($title); ?></h1>
+    <a href=""><i class="fa fa-file"></i> highmod-1.2.3b6-1.20.2-dev.jar</a>
+</body>
+</html>
